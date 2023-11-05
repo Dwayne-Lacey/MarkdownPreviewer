db68e8f39e361e9d36153219dec62b1c010dc9ab
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2023-11-05 11:48:39 -0500

    Added markdown slice

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import './index.css';
+
+const container = document.getElementById('root');
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+container);
+
